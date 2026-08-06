ef9aed2fdc818de32ac383181ecb2e33fbad9b82
Mahmoud Adel <mahmoud.adel0221@gmail.com>
2026-08-06 17:33:08 +0300

    some updated in MainActivity.kt to fix the issue with the card clash game. Added new video recording of the gameplay for reference.

--- android/xml/Day-4/CardClash2P/app/src/main/java/com/example/cardclash2p/MainActivity.kt
+++ android/xml/Day-4/CardClash2P/app/src/main/java/com/example/cardclash2p/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cardclash2p
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -51,25 +52,37 @@ class MainActivity : AppCompatActivity() {
 
             }else if(turn == 2){
                 card2 = randomInt
-                player_turns_tv.text = "Player 1's Turn"
                 turn++
                 draw_card_btn.text = "Go to round ${++round}"
                 changeCardNumber(card2_iv,randomInt)
+                player_turns_tv.setTextColor(Color.parseColor("#FFB13B"))
+                if(card1 > card2){
+                    player_turns_tv.text = "Player 1 Wins!! \nالف مبروك يخويا انت اللي كسبت الراوند دي"
+                    score1 += 10
+                }else if( card2 > card1 ){
+                    player_turns_tv.text = "Player 2 Wins!! \nالف مبروك يخويا انت اللي كسبت الراوند دي"
+                    score2 += 10
+                }else {
+                    score1 += 10 ; score2 += 10
+                    player_turns_tv.text = "تعادل يخواتي الراوند دي ):"
 
-                if(card1 > card2)score1 += 10
-                else if( card2 > card1 )score2 += 10
-                else {score1 += 10 ; score2 += 10}
+                }
                 updateScores(player_1_score_tv,player_2_score_tv,score1,score2)
                 draw_card_btn
 
 
                 if( round == 6 ){
                     if(score1 > score2){
+
                         draw_card_btn.text = "Player 1 is Wineer الف مبروك يخويا"
+                        player_turns_tv.text = "We Have a wineer congrats Player 1"
                     }else if(score2 > score1){
                         draw_card_btn.text = "Player 2 is Wineer الف مبروك يخويا"
+                        player_turns_tv.text = "We Have a wineer..\n\nCongrats Player 2!"
+                        player_turns_tv.setTextColor(Color.parseColor("#FFDDB5"))
                     }else{
                         draw_card_btn.text = "متعادلين يخواتي مش لعبة اللي هتخسرنا بعض"
+                        player_turns_tv.text = "Draw :)"
                     }
                     round = 1
                 }
@@ -78,6 +91,8 @@ class MainActivity : AppCompatActivity() {
                 card1_iv.setImageResource(R.drawable.question_mark_card)
                 card2_iv.setImageResource(R.drawable.question_mark_card)
                 draw_card_btn.text = "Player 1's Turn"
+                player_turns_tv.setTextColor(Color.parseColor("#22D3EE"))
+                player_turns_tv.text = "Player 1's Turn"
                 round_tv.text = "$round/5"
             }
         }
