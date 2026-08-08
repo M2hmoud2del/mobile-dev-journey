7623e9e1eeaba775246ab1e18a6f02bbcbee3341
Mahmoud Adel <mahmoud.adel0221@gmail.com>
2026-08-08 23:57:26 +0300

    add: implemented a simple Android application to demonstrate the use of XML layouts and basic navigation between activities. The app includes a main activity and a home activity, with appropriate resources and configurations set up for a smooth user experience.

--- android/xml/Day-5/app/src/main/java/com/example/mca005/MainActivity.kt
+++ android/xml/Day-5/app/src/main/java/com/example/mca005/MainActivity.kt
@@ -3,35 +3,35 @@ package com.example.mca005
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btn: Button = findViewById(R.id.website_btn)
-        val nameTV: TextView = findViewById(R.id.name_tv)
-
-        val receivedName = intent.getStringExtra("name")
-        nameTV.text = "Hello, $receivedName"
-
+        val btn: Button = findViewById(R.id.home_btn)
+        val nameET: EditText = findViewById(R.id.name_et)
 
         btn.setOnClickListener {
-            val website = "https://evil.com".toUri()
-            val i = Intent(Intent.ACTION_VIEW, website)
+            val name = nameET.text.toString()
+
+            val i = Intent(this, HomeActivity::class.java).apply {
+                putExtra("name", name)
+            }
             startActivity(i)
+
+            finish()
         }
     }
 }
