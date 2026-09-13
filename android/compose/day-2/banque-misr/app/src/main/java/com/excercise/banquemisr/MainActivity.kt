b22fb8ee50231062358c559d2f9bdc981a087cb6
Mahmoud Adel <capoking3@gmail.com>
2026-09-13 20:22:01 +0300

    add: Banque Misr App source code

--- android/compose/day-2/banque-misr/app/src/main/java/com/excercise/banquemisr/MainActivity.kt
+++ android/compose/day-2/banque-misr/app/src/main/java/com/excercise/banquemisr/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.excercise.banquemisr.ui.theme.BanqueMisrTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanqueMisrTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +36,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Image(
+                painter = painterResource(R.drawable.bm_icon),
+                contentDescription = stringResource(R.string.bank_masr_image_cd)
+            )
+            Text(
+                text = "العربية"
+            )
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(device = "spec:width=411dp,height=891dp", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+private fun LoginScreenPreview() {
     BanqueMisrTheme {
-        Greeting("Android")
+        LoginScreen()
     }
 }
